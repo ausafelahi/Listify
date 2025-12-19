82854bd48abedbb2dadbf339b28da4e9921a2a49
ausafelahi <ausafelahi8@gmail.com>
2025-12-19 23:34:19 +0500

    Listify v2.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/theme-provider";
 
 export const metadata: Metadata = {
   title: "Listify",
@@ -15,7 +16,16 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
-        <body>{children}</body>
+        <body>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
